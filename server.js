3434a6b14ef7175dcecf9c563a10733b511fb0c4
ynnarg <86639904+ynnarg@users.noreply.github.com>
2021-10-12 17:03:54 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 
+// Axios
+
+const axios = require("axios");
+
 // Express
 
+console.log("doe");
 const express = require("express");
+console.log("tihs");
 const app = express();
 app.use(express.json());
 
+// Variables
+
+let voteURL = "https://games.roblox.com/v1/games/votes?universeIds=";
+let placeIdToUniverseURL = "https://api.roblox.com/universes/get-universe-containing-place?placeid=";
+let idCache = new Map();
+
 // Express Listeners
 
-app.get("/", (req, res) => {
-    let body = req.body;
+app.get("/:id", (req, res) => {
+    let id = req.params.id;
     
+    if (!idCache.get(id.toString())) {
+      axios({
+        method: "get",
+        url: placeIdToUniverseURL + req.params.id
+      }).then((response) => {
+        let data = response.data.UniverseId;
+        idCache.set(id.toString(), data);
+        
+        axios({
+          method: "get",
+          url: voteURL + data
+        }).then((response) => {
+          res.send(response.data.data[0]);
+        });
+      });
+    } else {
+        axios({
+          method: "get",
+          url: voteURL + idCache.get(id.toString())
+        }).then((response) => {
+          res.send(response.data.data[0]);
+        });
+    }
+  
+  return "OK!"
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("yep");
 });
